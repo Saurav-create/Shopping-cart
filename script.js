e9e9a36b78a5389a54cfe8fa90e8433a9f4dd3f3
Saurav Datta <sauravdatta06@gmail.com>
2021-08-08 19:51:50 +0600

    2nd js update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 let mouseBtn = document.querySelector('#mouseBtn');
 let keyboardBtn = document.querySelector('#keyboardBtn');
-
+let ol = document.querySelector('#cart');
 
 let i = 1;
 
@@ -21,10 +21,18 @@ let i = 1;
 
 
 mouseBtn.addEventListener('click', addMouse);
+ol.addEventListener('click',removeMouse);
+keyboardBtn.addEventListener('click',addKeyboard);
 
 
 
 //functions
+
+
+
+
+
+
 
 function addMouse(e) {
     let innerCart = document.querySelector('#cart');
@@ -36,13 +44,17 @@ function addMouse(e) {
         let ol = document.querySelector('#cart');
         li.appendChild(document.createTextNode(`Mouse  Quantity ${i}`));
         mouseRemoveBtn.innerText = 'Remove';
-        mouseRemoveBtn.className = 'mouseRemove';
         li.appendChild(mouseRemoveBtn);
-        li.className = 'mouseli'
+        li.className = 'mouseli';
         ol.appendChild(li);
         console.log("this is from if");
-        document.querySelector(".mouseRemove").addEventListener('click', removeMouse);
-        // console.log(innerCart.childNodes);
+
+       
+
+        // mouseRemove.addEventListener('click', );
+
+
+        i = i + 1;
     }
     else {
 
@@ -55,35 +67,75 @@ function addMouse(e) {
         li.appendChild(mouseRemoveBtn);
         li.className = 'mouseli'
         ol.appendChild(li);
-
-
+        i = i + 1;
         console.log("this is from else");
     }
 
 
-    
 
 
-    i = i + 1;
+
+
     e.preventDefault();
 }
 
 
-// function removeMouse(e) {
-
-//     i=i-1;
-//     innerCart.removeChild(document.querySelector(".mouseli"));
-
-//     let li = document.createElement('li');
-//     let mouseRemoveBtn = document.createElement('button');
-//     let ol = document.querySelector('#cart');
-//     li.appendChild(document.createTextNode(`Mouse  Quantity ${i}`));
-//     mouseRemoveBtn.innerText = 'Remove';
-//     li.appendChild(mouseRemoveBtn);
-//     li.className = 'mouseli'
-//     ol.appendChild(li);
+function removeMouse(e) {
+    console.log(e.target.hasAttribute('href'));
+    let ele = e.target.parentElement;
+        ele.remove();
+        i=1;
+    e.preventDefault();
+}
 
 
 
-//         e.preventDefault();
-//     }
+
+
+
+
+
+function addKeyboard(e) {
+    let innerCart = document.querySelector('#cart');
+
+
+    if (innerCart.innerHTML == '') {
+        let li = document.createElement('li');
+        let mouseRemoveBtn = document.createElement('button');
+        let ol = document.querySelector('#cart');
+        li.appendChild(document.createTextNode(`Keyboard  Quantity ${i}`));
+        mouseRemoveBtn.innerText = 'Remove';
+        li.appendChild(mouseRemoveBtn);
+        li.className = 'mouseli';
+        ol.appendChild(li);
+        // console.log("this is from if");
+
+       
+
+        // mouseRemove.addEventListener('click', );
+
+
+        i = i + 1;
+    }
+    else {
+
+        innerCart.removeChild(document.querySelector(".mouseli"));
+        let li = document.createElement('li');
+        let mouseRemoveBtn = document.createElement('button');
+        let ol = document.querySelector('#cart');
+        li.appendChild(document.createTextNode(`Keyboard  Quantity ${i}`));
+        mouseRemoveBtn.innerText = 'Remove';
+        li.appendChild(mouseRemoveBtn);
+        li.className = 'mouseli'
+        ol.appendChild(li);
+        i = i + 1;
+        console.log("this is from else");
+    }
+
+
+
+
+
+
+    e.preventDefault();
+}
